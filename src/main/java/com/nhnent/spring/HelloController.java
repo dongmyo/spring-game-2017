e8e005b48c27c392f9c5e7c3d1524166ac30ec52
신동민/NE <dongmyo@nhnent.com>
2017-07-31 04:51:36 +0900

    * ModelAndView, Model, ModelMap, ...

--- src/main/java/com/nhnent/spring/HelloController.java
+++ src/main/java/com/nhnent/spring/HelloController.java
@@ -3,13 +3,17 @@ package com.nhnent.spring;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
     // TODO: name 파라미터로 전달받은 이름이 출력되도록 수정하시오.
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello() {
-        return "hello";
+    public ModelAndView hello(@RequestParam(name = "name") String name) {
+
+        return new ModelAndView("hello")
+                .addObject("name", name);
     }
 
 }
